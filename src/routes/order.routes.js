a89799cc678ed7e83baa1e23826094beb828c867
Mayank Kumar Sharma <mayankece155@gmail.com>
2024-05-12 18:01:08 +0530

    api key for test

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const crypto = require('crypto')
+const jsSHA = require('jssha')
 require('dotenv').config()
 
 router.post('/hashing', async (req, res) => {
@@ -15,12 +16,15 @@ router.post('/hashing', async (req, res) => {
   let { merchant_Key, salt } = process.env
 
   const data = {
-    key: process.env.merchant_Key,
+    key: merchant_Key,
     txnid: transitionId,
     customerName,
     amount: parseInt(amount),
     productInfo,
     customerId,
+    email: 'test@gmail.com',
+    firstname: 'firstname',
+    lastname: 'lastname',
     udf1: 'details1',
     udf2: 'details2',
     udf3: 'details3',
@@ -30,14 +34,21 @@ router.post('/hashing', async (req, res) => {
 
   console.log(data)
 
-  const cryptoResult = crypto.createHash('sha512')
+  const string = `${data.key}|${data.txnid}|${data.amount}|${data.customerName}|${data.email}|${data.firstname}|${data.lastname}|${data.productInfo}|${data.udf1}|${data.udf2}|${data.udf3}|${data.udf4}|${data.udf5}||||||${salt}`
+  const sha = new jsSHA('SHA-512', 'TEXT')
 
-  const string = `${data.key}|${data.txnid}|${data.amount}|${data.customerName}|${data.productInfo}|${data.udf1}|${data.udf2}|${data.udf3}|${data.udf4}|${data.udf5}||||||${data.salt}`
+  sha.update(string)
+
+  //Getting hashed value from sha module
+  const hash = sha.getHash('HEX')
+
+  // const cryptoResult = crypto.createHash('sha512')
+
   console.log(string)
-  cryptoResult.update(string)
+  // cryptoResult.update(string)
 
-  const hash = cryptoResult.digest('hex')
-  console.log(hash)
+  // const hash = cryptoResult.digest('hex')
+  // console.log(hash)
 
   return res.status(201).send({
     hash,
